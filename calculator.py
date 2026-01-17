1cd7513cf40d48388bd664ca1877bed279c9ea6a
aullanisa <rifqiaulannisa@gmail.com>
2026-01-17 16:37:56 -0500

    add functions to calculator

--- calculator.py
+++ calculator.py
@@ -9,18 +9,3 @@ def subtract(a,b):
 
 def divide(a,b):
 	return a/b
-
-
-
-def square(a):
-	return a * a
-
-
-def cube(a):
-	return a * a * a
-
-
-if __name__ == "__main__":
-	print("I'm going to use the calculator functions to multiply 5 and 6")
-	x = multiply(5, 6)
-	print(x)
