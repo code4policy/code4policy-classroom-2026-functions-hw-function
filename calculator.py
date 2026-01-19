00d09a48863ab7d7a05372512e2092c9d0b8271d
cvanderwood <clairejvanderwood@gmail.com>
2026-01-19 15:50:42 -0500

    print result of functions

--- calculator.py
+++ calculator.py
@@ -10,6 +10,21 @@ def subtract(a,b):
 def divide(a,b):
 	return a/b
 
+print("I'm going use the calculator functions to multiply 10 and 2")
+c=multiply(10,2)
+print(c)
+
+print("I'm going use the calculator functions to add 10 and 2")
+d = add(10,2)
+print(d)
+
+print("I'm going use the calculator functions to subtract 10 and 2")
+e = subtract(10,2)
+print(e)
+
+print("I'm going use the calculator functions to divide 10 and 2")
+f = divide(10,2)
+print(f)
 
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
