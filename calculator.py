e3d14fd93d09509b133c4413de97a0e06ec8b50d
cvanderwood <clairejvanderwood@gmail.com>
2026-01-19 15:45:07 -0500

    add functions to calculator

--- calculator.py
+++ calculator.py
@@ -14,3 +14,15 @@ def divide(a,b):
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+print("I'm going use the calculator functions to add 5 and 6")
+y = add(5,6)
+print(y)
+
+print("I'm going use the calculator functions to subtract 5 and 6")
+a = subtract(5,6)
+print(a)
+
+print("I'm going use the calculator functions to divide 5 and 6")
+b = divide(5,6)
+print(b)
